095ac4e1d9b281967594b2cda567991910d6e3cc
iamlucasmateo <perrone.lucasmateo@gmail.com>
2022-05-02 17:34:42 -0300

    finished pytest intro

--- tests/test_gist.py
+++ tests/test_gist.py
@@ -24,3 +24,40 @@ def test_xfail():
 @pytest.mark.slow
 def test_slow():
     assert 1 == 1
+
+class Company:
+    def __init__(self, name: str, stock_symbol: str):
+        self.name = name
+        self.stock_symbol = stock_symbol
+    
+    def __str__(self):
+        return f"{self.name}:{self.stock_symbol}"
+
+# Fixtures
+@pytest.fixture
+def company() -> Company:
+    return Company(name="Fiver", stock_symbol="FVRR")
+
+# the company parameter will be taken from the fixtures
+def test_with_fixture(company):
+    print(f"Printing {company} from fixture")
+
+
+# Parametrize
+@pytest.mark.parametrize(
+    "company_name",   
+    ["TikTok", "Instagram", "Discord"],
+    ids=["TIKTOK_TEST", "INSTAGRAM_TEST", "DISCORD_TEST"]
+)
+def test_with_parameters(company_name): # argument from parameter
+    print(f"Parameter: {Company(company_name, 'XXX')}")
+
+
+# Testing exceptions
+def raise_exc(): 
+    raise ValueError("This is an exception")
+
+def test_raise_exepction():
+    with pytest.raises(ValueError) as e:
+        raise_exc()
+    assert "This is an exception" == str(e.value)
